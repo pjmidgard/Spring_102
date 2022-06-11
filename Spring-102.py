3d4cee3dd2bfa517cab5dac1a1973bd64b66f134
Java Software <pj.midgard@yahoo.com>
2022-06-11 18:22:43 +0300

    Update Spring-102.py

--- Spring-102.py
+++ Spring-102.py
@@ -141,27 +141,10 @@ class compression:
 
                        # Read the whole file at once
                         data = binary_file.read()
-                        if Portal==9 and data[0:3]!=b'\xff\xd8\xff':
-                        	print("Program close because this is file incorrect")
-                        	raise SystemExit
-                        if Portal==9 and data[0:3]==b'\xff\xd8\xff': 
-                        	data=data[3:]
-                        if Portal==7 and data[0:4]!=b'\x89\x50\x4e\x47' :
-                        	print("Program close because this is file incorrect")
-                        	raise SystemExit
-                        if Portal==7 and data[0:4]==b'\x89\x50\x4e\x47' :             
-                        	data=data[4:]
-                        if Portal==8 and data[0:11]!=b'\x00\x00\x00\x18\x66\x74\x79\x70\x6d\x70\x34':
-                        	print("Program close because this is file incorrect")
-                        	raise SystemExit
-                        if Portal==8 and data[0:11]==b'\x00\x00\x00\x18\x66\x74\x79\x70\x6d\x70\x34':             
-                        	data=data[11:]
+                 
                        
-                        import zstandard
-                        data=zstandard.compress(data)
 
-                        if  data [0:4] == b'\x28\xb5\x2f\xfd':
-                            data=data[4:]   
+                  
                         s=str(data)
                        
                         lenf1=len(data)
@@ -540,18 +523,7 @@ class compression:
                     nac=len(nameas)
                     nameas=name[:nac-11]
                     nac=len(nameas)
-                    if nameas[nac-5:nac]==".docx":
-                    	Portal=1
-                    if nameas[nac-4:nac]==".pdf":
-                    	Portal=3
-                    if nameas[nac-4:nac]==".doc":
-                    	Portal=1
-                    if nameas[nac-4:nac]==".png":
-                    	Portal=7
-                    if nameas[nac-4:nac]==".mp4":
-                    	Portal=8
-                    if nameas[nac-4:nac]==".jpg":
-                    	Portal=9
+            
                     
                     
                     nac=len(nameas)
@@ -940,19 +912,11 @@ class compression:
                                     qqwslenf="%0"+qqwslenf+"x"
                              
                                     jl=binascii.unhexlify(qqwslenf % n)
-                                    import zstandard
-                                  
-                                    jl=b'\x28\xb5\x2f\xfd'+jl
-                                    jl=zstandard.decompress(jl) 
+                               
                                     sssssw=len(jl) 
                                     sssssw=len(jl)
                                    
-                                    if Portal==7:
-                                        jl= b'\x89\x50\x4e\x47'+jl
-                                    if Portal==8:
-                                    	                    jl=b'\x00\x00\x00\x18\x66\x74\x79\x70\x6d\x70\x34'+jl
-                                    if Portal==9:
-                                    	                    																		jl=b'\xff\xd8\xff'+jl
+
                                     qqqwz=qqqwz+1
                                     szxzzza=""
                                     szxzs=""
